4b86669045eaa80a26ca78388e6c056fde0f22c1
walker <walkerchan2019@163.com>
2022-11-12 20:51:51 +0800

    leetcode-28

--- 1v1/1111/04.js
+++ 1v1/1111/04.js
@@ -3,15 +3,28 @@
  * @param  {string} pattern
  */
 
-function findSubstr(source, pattern) {
+function findSubstr(source, pattern) { 
   //增添：pattern 里的字母完全没有重复，如何优化 ？
   let j = 0;
+  let note=0
   for (let i = 0; i < source.length; i++) {
+    console.log('11');
     if (source[i] !== pattern[j]) {
-      j = 0;
-      if (source[i] === pattern[j]){i++;} 
+      if (note>=1 ) {
+        i = i - (note - 1)
+        j = 0;
+        note = 0;
+      } else {
+        j = 0;
+        note = 0;
+      }
+      if (source[i] === pattern[j]) {
+        j++ // 这里是j++ 
+        note++
+      }
     } else {
       j++;
+      note ++ 
     }
     if (j === pattern.length) {
       return i - j + 1;
@@ -19,6 +32,11 @@ function findSubstr(source, pattern) {
   }
   return -1;
 }
+
+// findSubstr("abcdef","bcd")
+// console.log(findSubstr("ababcd", "abc"));
+
+// console.log(findSubstr("mississippi", "issip"))
 
 module.exports = {
   findSubstr,
--- 1v1/1111/__test__/index.spec.js
+++ 1v1/1111/__test__/index.spec.js
@@ -1,4 +1,4 @@
-const { findSubstr } = require("../02.js");
+const { findSubstr } = require("../04.js");
 
 describe("findSubstr", () => {
   test("success", () => {
@@ -7,7 +7,13 @@ describe("findSubstr", () => {
   });
 
   test("fail", () => {
-    expect(findSubstr("abcd", "abc")).toBe(0);
-    expect(findSubstr("ababcd", "abc")).toBe(2); //构造一个fail的例子
+    // expect(findSubstr("abcd", "abc")).toBe(0);
+    // expect(findSubstr("ababcd", "abc")).toBe(2); //构造一个fail的例子
+    // expect(findSubstr("aabcdabcddefghijklmn", "abcd")).toBe(1);
+
+
+    expect(findSubstr("mississippi", "issip")).toBe(4);
+    // "mississippi"
+    // ("issip");  
   });
 });
